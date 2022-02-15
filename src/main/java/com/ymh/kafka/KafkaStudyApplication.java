0b7df1b6307b494a1e30e0bac163d43a0ddf618b
YUN <yunmyeonghun@gmail.com>
2022-02-15 16:15:30 +0900

    study 01 Setup application & kafka config

--- src/main/java/com/ymh/kafka/KafkaStudyApplication.java
+++ src/main/java/com/ymh/kafka/KafkaStudyApplication.java
@@ -1,4 +1,4 @@
-package com.example.kafkastudy;
+package com.ymh.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ymh/kafka/KafkaStudyApplicationTests.java
+++ src/test/java/com/ymh/kafka/KafkaStudyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.kafkastudy;
+package com.ymh.kafka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
