01448b8e95fc554342b31ff689d9304f039aed72
YUN <yunmyeonghun@gmail.com>
2022-02-17 12:42:24 +0900

    study 05 Msg send controller

---
+++ src/main/java/com/ymh/kafka/MessageController.java
@@ -0,0 +1,22 @@
+package com.ymh.kafka;
+
+import lombok.AllArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/messages")
+@AllArgsConstructor
+public class MessageController {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping("publish")
+    public void publish(@RequestBody MessageRequest request) {
+        kafkaTemplate.send("NEWS", request.message);
+    }
+
+}
---
+++ src/main/java/com/ymh/kafka/MessageRequest.java
@@ -0,0 +1,8 @@
+package com.ymh.kafka;
+
+import lombok.Data;
+
+@Data
+public class MessageRequest {
+    String message;
+}
